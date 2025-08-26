a4a4e0c90f1047cfd4d12991458d7e3467f6a83b
kiranR007 <kirankiru363@gmail.com>
2025-08-26 13:04:54 +0530

    vpattern

---
+++ v_pattern.py
@@ -0,0 +1,11 @@
+ch = str(input())
+spaces = 8
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end="")
+    for k in range(1,spaces+1):
+        print(end=" ")
+    spaces = spaces-2
+    for j in range(i,0,-1):
+        print(j,end="")
+    print()
