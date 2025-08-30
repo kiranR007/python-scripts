3dfeebd68d5a55324fd3a76adbb7b9fd99c137aa
kiranR007 <kirankiru363@gmail.com>
2025-08-30 17:00:23 +0530

    duplicate charecter

---
+++ duplicate_charecter.py
@@ -0,0 +1,14 @@
+t=int(input())
+while(t!=0):
+    str = input()
+    freq = [0]*26
+    for i in range(len(str)):
+        freq[ord(str[i])-97]+=1
+    flag = 0
+    for i in range(26):
+        if freq[i]>1:
+            flag=1
+            print("{}={}".format(chr(i+97),freq[i]),end=" ")
+    if flag==0:
+        print("-1")
+    t-=1
